e15c403208ac0c5335c715b486648561cb4c2e89
Khaoula Elmajni <elmajnikhaoula99@gmail.com>
2022-12-19 13:24:28 +0100

    1st project

---
+++ digital/src/test/java/me/elmajni/digital/DigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elmajni.digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
